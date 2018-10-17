8c3e2c12c6497295e2e89daf862dac9a94ce5428
Devin Sweeting <devinsweeting@gmail.com>
2018-10-16 21:29:59 -0700

    finish flashcards

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,48 @@
     $("#op-show").slideToggle();
   });
 
-  $(".click3").click(function() {
-    $("#vr-hide").slideToggle();
-    $("#vr-show").slideToggle();
+  $(".click4").click(function() {
+    $("#vnc-hide").slideToggle();
+    $("#vnc-show").slideToggle();
+  });
+
+  $(".click5").click(function() {
+    $("#f-hide").slideToggle();
+    $("#f-show").slideToggle();
+  });
+
+  $(".click6").click(function() {
+    $("#m-hide").slideToggle();
+    $("#m-show").slideToggle();
+  });
+
+  $(".click7").click(function() {
+    $("#arg-hide").slideToggle();
+    $("#arg-show").slideToggle();
+  });
+
+  $(".click8").click(function() {
+    $("#p-hide").slideToggle();
+    $("#p-show").slideToggle();
+  });
+
+  $(".click9").click(function() {
+    $("#rtn-hide").slideToggle();
+    $("#rtn-show").slideToggle();
+  });
+
+  $(".click10").click(function() {
+    $("#cm-hide").slideToggle();
+    $("#cm-show").slideToggle();
+  });
+
+  $(".click11").click(function() {
+    $("#str-hide").slideToggle();
+    $("#str-show").slideToggle();
+  });
+
+  $(".click12").click(function() {
+    $("#b-hide").slideToggle();
+    $("#b-show").slideToggle();
   });
  });
